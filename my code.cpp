0a3e516bafca47080744de2abc3acc765a297d00
Jitendrasharma101 <97757389+Jitendrasharma101@users.noreply.github.com>
2022-01-19 00:19:11 +0530

    Create my code.cpp

---
+++ my code.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int i,j,row,column;
+  cin>>row>>column;
+  for(i=0;i<row;i++)
+  { for(j=0;j<column;j++)
+  {
+    cin>>arr[i][j];
+  }
+  }
+  for(i=0;i<row;i++0)
+  {
+    for(j=0;j<column;j++)
+    {
+      cout<<arr[i][j];
+    }
+  }
+
+}
